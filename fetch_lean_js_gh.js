03250dcd56083fff332c4ccdee799af4c75ff1d6
E.W.Ayers <E.W.Ayers@maths.cam.ac.uk>
2020-07-08 18:49:02 +0100

    now gets the latest lean release

--- fetch_lean_js_gh.js
+++ fetch_lean_js_gh.js
@@ -19,11 +19,11 @@ const access_token = fs.readFileSync('secrets/github_access_token.txt', 'utf8').
 const octokit = new Octokit({
     auth : access_token
 });
-const options = {owner : "EdAyers", repo : "lean"};
+const options = {owner : "leanprover-community", repo : "lean"};
 const out_dir = "./dist"
 const branch = "widget"
 
-async function * getAllRuns() {
+async function * getAllActionsRuns() {
     console.log("Getting workflow runs");
     let response = await octokit.actions.listRepoWorkflowRuns({
         ...options,
@@ -38,14 +38,49 @@ async function * getAllRuns() {
     }
 }
 
-async function run() {
+async function getFromRelease(options) {
+    const {owner, repo, tag} = options;
+    const release = tag ? (await octokit.repos.getReleaseByTag({owner, repo, tag})) : (await octokit.repos.getLatestRelease({owner, repo}));
+    console.log(`Found release ${release.data.name}`);
+    const assets = release.data.assets;
+    const asset = assets.find(x => x.name.endsWith("--browser.zip"));
+    if (!asset) {
+        throw new Error("Failed to find browser asset from these:", assets);
+    }
+    const id = asset.id;
+    // const url = await octokit.repos.getReleaseAsset()
+    const url = asset.browser_download_url;
+    // curl -u my_client_id:my_client_secret
+
+    if (!fs.existsSync(out_dir)) {
+        await fs.promises.mkdir(out_dir);
+    }
+    const shell_file = path.join(out_dir, "shell.zip");
+
+    await sh(`curl -L -u ${user}:${access_token} -o ${shell_file} "${url}"`);
+    const items = [
+        "lean_js_js.js",
+        "lean_js_wasm.js",
+        "lean_js_wasm.wasm"
+    ];
+    for (let item of items) {
+        const input_ext = path.join("build", "shell", item)
+        const out_file = path.join(out_dir, "shell", item);
+        // -o = overwrite files without giving a prompt.
+        await sh(`unzip -p -o ${shell_file} ${input_ext} > ${out_file}`);
+    }
+
+    console.log("Done")
+}
+
+async function getFromActionsArtifact() {
     const branch_resp = await octokit.repos.getBranch({...options, branch});
     const head_sha = branch_resp.data.commit.sha;
     console.log(`Branch: ${branch}`);
     console.log(`SHA: ${head_sha}`);
     console.log(`Message: ${branch_resp.data.commit.commit.message}`);
     let run_id;
-    for await (let run of getAllRuns()) {
+    for await (let run of getAllActionsRuns()) {
        if (run.head_sha === head_sha) {
          console.log(`Using run ${run.url}`);
          run_id = run.id;
@@ -63,6 +98,10 @@ async function run() {
             break;
         }
     }
+    if (!artifact_url) {
+        console.log("no artefact found, github sometimes deletes artifacts so try running the action again.");
+        return;
+    }
     if (!fs.existsSync(out_dir)) {
         await fs.promises.mkdir(out_dir);
     }
@@ -70,23 +109,6 @@ async function run() {
 
     await sh(`curl -v -L -u ${user}:${access_token} -o ${shell_file} "${artifact_url}/zip"`);
     console.log(`Downloaded to ${shell_file}`);
-
-    // [note] I couldn't get this to work so I just exed shell commands instead.
-    // const shell_writer = fs.createWriteStream(shell_file);
-    // let dl = await octokit.actions.downloadArtifact({...options, artifact_id, archive_format : "zip"});
-    // console.log(`Downloading from ${dl.url}`);
-    // let stream;
-    // await new Promise((resolve, reject) => {
-    //     http.request({
-    //         url : dl.url,
-    //         method : "GET",
-    //         encoding : null,
-    //     }, resp => {
-    //         console.log(`Saving to ${shell_file}`);
-    //         resp.pipe(shell_writer).on("end", resolve);
-    //     });
-    // });
-    // console.log(`Saved`)
 
     const items = [
         "lean_js_js.js",
@@ -102,4 +124,4 @@ async function run() {
     console.log("Done");
 }
 
-run();
+getFromRelease(options);
